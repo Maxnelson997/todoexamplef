9d64b0b1467af74d362aa45cced011ba5e03998c
Max <maxnelson997@gmail.com>
2018-12-05 14:17:40 -0700

    grid and todos" "

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 
+const Todo = () => {
+    return (
+        <div className='todos__todo'>
+            <h5>title</h5>
+            <button>complete</button>
+        </div>
+    )
+}
+
 class Todos extends Component {
    render() {
        const { className } = this.props
        return (
             <div className={`${className} todos`}>
-               
+               <Todo/>
+               <Todo/>
+               <Todo/>
+               <Todo/>
+               <Todo/>
+               <Todo/>
             </div>
        )
    }
